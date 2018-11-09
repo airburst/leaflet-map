5dbb79f97d23f7c0ae5fa0ac07f6c0a43bc82859
airburst <mark@fairhursts.net>
2018-11-09 21:53:56 +0000

    feat: Added custom icon

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
-import { Map, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, Circle, Polyline } from 'react-leaflet';
+import L from 'leaflet';
 import './Map.css';
+
+var myIcon = L.icon({
+  iconUrl: '../assets/marker-person.png',
+  iconSize: [64, 93],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
 
 const LeafletMap = props => {
   const [lat, setLat] = useState(51.41715608736272);
@@ -21,14 +29,14 @@ const LeafletMap = props => {
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-      <Marker position={position}>
-        <Popup>
-          This is a popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={position} icon={myIcon}>
+        <Popup>Mark</Popup>
       </Marker>
-      <Marker position={marker2} />
 
       <Circle center={position} radius={500} fillColor="red" color="red" />
+
+      {/* <Marker position={marker2} /> */}
+      {/* <Polyline positions={[position, marker2, marker3]} color="red" /> */}
     </Map>
   );
 };
